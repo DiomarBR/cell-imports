aa20a6a9bc690daf8f65ee7c0ce8eb59edaf295b
DiomarBR <diomarbr4@gmail.com>
2022-08-16 22:36:14 -0300

    update

--- scripts.js
+++ scripts.js
@@ -7,71 +7,59 @@ const videos = [
   {
     title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
     duration: "57 min",
-    thumb: "images/garda roupa 1.jpg",
+    thumb: "images/imagem 1.png",
     preço:"10"
   },
   {
     title: "Desvendando o CSS Grid na prática | Mayk Brito",
     duration: "36 min",
-    thumb: "images/guarda roupa 2 .jpg",
+    thumb: "images/imagem 2.png",
     preço:"10"
   },
   {
     title: "Array: Higher Order Functions | Mayk Brito",
     duration: "54 min",
-    thumb: "images/guarda roupa 3.jpg",
+    thumb: "images/imagem 3.png",
   },
   {
     title: "O que é API? REST e RESTful? | Mayk Brito",
     duration: "33 min",
-    thumb: "images/guarda roupa 4 .jpg",
+    thumb: "images/imagem 4.png",
   },
   {
     title: "Desvendando a variável this no Javascript | Mayk Brito",
     duration: "48 min",
-    thumb: "images/guarda roupa 5.jpg",
+    thumb: "images/imagem 5.png",
   },
   {
     title:
       "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
     duration: "33 min",
-    thumb: "images/guarda roupa 7.jpg",
+    thumb: "images/imagem 6.png",
   },
   {
     title:
       "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
     duration: "33 min",
-    thumb: "images/imagen 8.jpg",
+    thumb: "images/imagem 7.png",
   },
   {
     title:
       "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
     duration: "33 min",
-    thumb: "images/imagen 9.jpg",
+    thumb: "images/imagem 8.png",
   },
   {
     title:
       "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
     duration: "33 min",
-    thumb: "images/imagen 10.jpg",
+    thumb: "images/imagem 9.png",
   },
   {
     title:
       "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
     duration: "33 min",
-    thumb: "images/imagem 11.jpg",
-  },
-  {
-    title:
-      "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
-    duration: "33 min",
-    thumb: "images/imagem 12.jpg",
-  },
-  {
-    title:
-      "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
-    duration: "33 min",
-    thumb: "images/imagem 13.jpg",
+    thumb: "images/imagem 10.png",
   },
 ];
 
